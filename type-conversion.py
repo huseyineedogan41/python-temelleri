1605e9c80c63e9eee0d881253e7fe69c6eeea191
huseyineedogan <huseyin_8519@hotmail.com>
2021-10-07 21:30:36 +0300

    first commit

---
+++ type-conversion.py
@@ -0,0 +1,27 @@
+
+# x=input("1.sayı:")
+# y=input("2.sayı:")
+
+
+# print(type(x))
+# print(type(y))
+
+
+# toplam=int(x)+int(y)
+
+
+# print(toplam)
+
+
+x=9                        #int                         
+y=15.8                     #flout
+name="huseyin"             #string
+isOnline=True              #bool
+
+# print(type(x))
+# print(type(y))
+# print(type(name))
+# print(type(isOnline))
+
+# type Conversion
+
---
+++ veritable.py
@@ -0,0 +1,13 @@
+maasAli=5000
+maasAhmet=4000
+vergi=0.27
+print(maasAli - (maasAli * vergi))
+print(maasAhmet- (maasAhmet * vergi))
+
+
+print(5000-(5000 * 0.27))
+print(10000-(10000 * 0.27))
+print(25000-(25000 * 0.27))
+print("huseyin")
+print("yurtbaşı koyü")
+#rakamla baslayamaz
